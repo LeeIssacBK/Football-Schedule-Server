bfd94d180d89a797ed1629e49609347ed8d6e642
issac <qodrp@naver.com>
2024-03-15 11:22:08 +0900

    fix auditing

--- serv/src/main/java/com/fs/configs/jpa/JpaConfig.java
+++ serv/src/main/java/com/fs/configs/jpa/JpaConfig.java
@@ -1,10 +1,43 @@
 package com.fs.configs.jpa;
 
+import com.fs.api.user.domain.User;
+import com.fs.api.user.dto.UserDto;
+import com.fs.api.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-@Configuration
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @EnableJpaAuditing
+@Configuration
 public class JpaConfig {
 
+    private final UserRepository userRepository;
+
+    @Bean
+    public AuditorAware<User> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+
+    public class AuditorAwareImpl implements AuditorAware<User> {
+        @Override
+        public Optional<User> getCurrentAuditor() {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (null == authentication || !authentication.isAuthenticated()) {
+                return Optional.empty();
+            }
+            if (authentication.getPrincipal() instanceof UserDto.Simple principal) {
+                return userRepository.findById(principal.getId());
+            }
+            return Optional.empty();
+        }
+    }
+
 }
+
--- serv/src/main/java/com/fs/configs/jpa/base/BaseDomain.java
+++ serv/src/main/java/com/fs/configs/jpa/base/BaseDomain.java
@@ -2,20 +2,19 @@ package com.fs.configs.jpa.base;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-public class BaseDomain implements Serializable {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseDomain implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +22,7 @@ public class BaseDomain implements Serializable {
 
     @CreatedDate
     @JsonSerialize(using = LocalDateTimeSerializer.class)
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @JsonSerialize(using = LocalDateTimeSerializer.class)
