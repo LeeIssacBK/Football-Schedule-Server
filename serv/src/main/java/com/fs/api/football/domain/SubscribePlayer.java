5b8d63fba3afa703057ba61d8051f2b35ca4860e
issac <qodrp@naver.com>
2024-03-19 15:43:24 +0900

    add subscribe entity

--- serv/src/main/java/com/fs/api/football/domain/SubscribePlayer.java
+++ serv/src/main/java/com/fs/api/football/domain/SubscribePlayer.java
@@ -1,10 +1,17 @@
 package com.fs.api.football.domain;
 
+import com.fs.api.user.domain.User;
 import com.fs.configs.jpa.base.BaseDomain;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class SubscribePlayer extends BaseDomain {
 
-    
+    @ManyToOne
+    private Player player;
+
+    @ManyToOne
+    private User user;
+
 }
--- serv/src/main/java/com/fs/api/football/domain/SubscribeTeam.java
+++ serv/src/main/java/com/fs/api/football/domain/SubscribeTeam.java
@@ -1,10 +1,17 @@
 package com.fs.api.football.domain;
 
+import com.fs.api.user.domain.User;
 import com.fs.configs.jpa.base.BaseDomain;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class SubscribeTeam extends BaseDomain {
 
+    @ManyToOne
+    private Team team;
+
+    @ManyToOne
+    private User user;
 
 }
