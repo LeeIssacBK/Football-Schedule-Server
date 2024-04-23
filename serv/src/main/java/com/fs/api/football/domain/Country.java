c7a4cd35162c02696a8055e3ed8479e3a4fb4f52
issac <qodrp@naver.com>
2024-04-23 11:34:10 +0900

    add country continent

--- serv/src/main/java/com/fs/api/football/domain/Country.java
+++ serv/src/main/java/com/fs/api/football/domain/Country.java
@@ -1,7 +1,10 @@
 package com.fs.api.football.domain;
 
+import com.fs.common.enums.Continent;
 import com.fs.configs.jpa.base.BaseDomain;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,5 +21,7 @@ public class Country extends BaseDomain {
     private String name;
     private String krName;
     private String flag;
+    @Enumerated(EnumType.STRING)
+    private Continent continent;
 
 }
---
+++ serv/src/main/java/com/fs/common/enums/Continent.java
@@ -0,0 +1,5 @@
+package com.fs.common.enums;
+
+public enum Continent {
+    EUROPE, ASIA, AFRICA, NORTH_AMERICA, SOUTH_AMERICA, OCEANIA
+}
