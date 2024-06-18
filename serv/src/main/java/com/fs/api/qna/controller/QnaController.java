efef56d6b91c194f91bc96b6ace38e68e9ce9879
issac <qodrp@naver.com>
2024-06-18 17:24:11 +0900

    fix qna request method post -> get

--- serv/src/main/java/com/fs/api/qna/controller/QnaController.java
+++ serv/src/main/java/com/fs/api/qna/controller/QnaController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class QnaController {
     private final QnaService qnaService;
 
     @Operation(summary = "Q & A 내용을 가져온다.")
-    @PostMapping
+    @GetMapping
     public ResponseEntity<List<QnaDto.Response>> get() {
         return ResponseEntity.ok(qnaService.get());
     }
