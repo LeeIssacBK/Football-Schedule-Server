04ae7969ad048eb7efc80ec1b2dbefaea5bc5c99
issac <qodrp@naver.com>
2024-07-25 15:05:37 +0900

    add application start log

--- serv/src/main/java/com/fs/Application.java
+++ serv/src/main/java/com/fs/Application.java
@@ -14,6 +14,7 @@ public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("Application start complete");
 	}
 
 	@Override
