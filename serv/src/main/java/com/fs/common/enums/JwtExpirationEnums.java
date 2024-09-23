baa6556e5ee512675c3a41c35e27abc57058fed4
issac <qodrp@naver.com>
2024-09-23 15:24:43 +0900

    fix access token expiration time

--- serv/src/main/java/com/fs/common/enums/JwtExpirationEnums.java
+++ serv/src/main/java/com/fs/common/enums/JwtExpirationEnums.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum JwtExpirationEnums {
-    ACCESS_TOKEN_EXPIRATION_TIME("1 day", 1000L * 60 * 60 * 24),
-    REFRESH_TOKEN_EXPIRATION_TIME("7 days", 1000L * 60 * 60 * 24 * 7);
+    ACCESS_TOKEN_EXPIRATION_TIME("7 day", 1000L * 60 * 60 * 24 * 7),
+    REFRESH_TOKEN_EXPIRATION_TIME("30 days", 1000L * 60 * 60 * 24 * 30);
     private String description;
     private long value;
 }
